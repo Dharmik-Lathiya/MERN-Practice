26d77a815c3c6bb967902ecd5a2af8900d75c64d
Dharmik-Lathiya <dharmiklathiya.it.com>
2025-04-30 23:33:49 +0530

    [A[BNode

--- JSON/script.js
+++ JSON/script.js
@@ -1,6 +1,7 @@
 let jsonRes = '{"fact":"The first cat show was in 1871 at the Crystal Palace in London.","length":63}';
 
 let ValidJson = JSON.parse(jsonRes);
+console.log(ValidJson);
 
 console.log('ValidJson', ValidJson);
 
